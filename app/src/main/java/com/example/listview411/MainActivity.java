e232be602aa83f897fdee47f548ce6ad24200364
Rodion <george.t.o.p@mail.ru>
2020-03-18 09:23:59 +0300

    Не показывает текст

--- app/src/main/java/com/example/listview411/MainActivity.java
+++ app/src/main/java/com/example/listview411/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.listview411;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
 SimpleAdapter simpleAdapter;
+
+    private List<Map<String, String>> prepareContent() {
+        String[] strings = getString(R.string.large_text).split("\n");
+        List<Map<String, String>> list = new ArrayList<>();
+        for (String string : strings) {
+            Map<String, String> map = new HashMap<>();
+            map.put("Заголовок", string);
+            map.put("Подзаголовок", String.valueOf(string.length()));
+            list.add(map);
+        }
+        return list;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +35,12 @@ SimpleAdapter simpleAdapter;
         setContentView(R.layout.activity_main);
         ListView listView = findViewById(R.id.listView);
 
+        List<Map<String, String>> values = prepareContent();
+        String[] from = {Source.KEY1, Source.KEY3};
+        int[] to = {R.id.nameTv, R.id.infoTv};
 
-        simpleAdapter = new SimpleAdapter(this,Source.data,R.layout.item_simple,
-                new String[]{Source.KEY1,Source.KEY2,Source.KEY3},new int[]{R.id.nameTv,R.id.placeTv,R.id.infoTv});
+        BaseAdapter listContentAdapter = new SimpleAdapter(this, values, R.layout.item_simple,from,to);
+        listView.setAdapter(listContentAdapter);
 
-
-
-        listView.setAdapter(simpleAdapter);
     }
 }
